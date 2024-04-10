1e9572282e0bbd31bd8ab75239b5dc8fc784cf7b
marcelofelipesilva <marcelofelipesilva@outlook.com>
2024-04-10 17:23:06 -0300

    criando  parametros e construtor da classe.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,20 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo= modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            
+            // TODO: Passar os parâmetros do construtor para as propriedades da classe
+            
         }
 
         public void Ligar()
